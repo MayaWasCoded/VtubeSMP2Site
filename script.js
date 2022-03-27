8dda00e477347b1b7004c70ff724abe5ae2d4bda
MayaWasCoded <101096304+MayaWasCoded@users.noreply.github.com>
2022-03-27 01:36:18 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -3,14 +3,12 @@ function redirect(){
 }
 function send(){
 
-    let file_system = new ActiveXObject("Scripting.FileSystemObject");
-    let enter = "<br>"
     let name = document.getElementById("name").value;
     let nick = document.getElementById("nick").value;
     let age = document.getElementById("age").value;
     let rules_accept = document.getElementById("accept").checked;
-    let file_system_open = file_system.OpenTextFile(C:\\Users\\User\\strona\\vtube smp\\"Test.txt", 8, True);
 
+    alert("congrats you are now with us!!");
     
     /*
     Email.send({
